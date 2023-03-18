cf4913a8298f2dffabd2134cb8d151c0c24b5ca9
phill9242 <philcarvalho@live.com>
2023-03-18 12:37:04 -0300

    finished ex00

--- module09/ex00/BitcoinExchange.cpp
+++ module09/ex00/BitcoinExchange.cpp
@@ -1,76 +1,145 @@
 #include "BitcoinExchange.hpp"
 
-std::map<std::string, double> readBitcoinPriceData(const std::string& filename) {
-    std::ifstream dataFile(filename.c_str());
-    std::map<std::string, double> bitcoinPriceData;
 
-    if (!dataFile.is_open()) {
-        return bitcoinPriceData;
-    }
-
-    std::string line;
-    while (std::getline(dataFile, line)) {
-        std::stringstream ss(line);
-        std::string date;
-        double value;
-
-        std::getline(ss, date, ',');
-        ss >> value;
-
-        bitcoinPriceData[date] = value;
-    }
-    dataFile.close();
-
-    return bitcoinPriceData;
+int   bitcoinExchange (char* fileName)
+{   
+	std::ifstream inputFile(fileName);
+	std::map<std::string, double> bitcoinMap = makeBitcoinMap("data.csv");
+	readInputFile (inputFile, bitcoinMap);
+	inputFile.close();
+	return (1);
 }
 
-std::string findClosestDate(const std::map<std::string, double>& data, const std::string& targetDate) {
-   
-    if (data.find(targetDate) != data.end()) {
-        return targetDate;
-    }
+void	readInputFile (std::ifstream &inputFile, std::map<std::string, double> bitcoinMap)
+{	
+	std::string line;
 
-    std::map<std::string, double>::const_iterator upperBound = data.upper_bound(targetDate);
+	while (std::getline(inputFile, line)) 
+	{
+		std::string date;
+		float value;
 
-    if (upperBound == data.begin()) {
-        return upperBound->first;
-    }
-    --upperBound;
-    std::cout << "retorno = " << upperBound->first << std::endl;
-    return upperBound->first;
+		if (validateInputLine(line, date, value)) {
+			std::string closestDate = findClosestDate(bitcoinMap, date);
+			double exchangeRate = bitcoinMap[closestDate];
+			double result = value * exchangeRate;
+			std::cout << date << " => " << value << " = " << result << '\n';
+		}
+	}
+	return ;
 }
 
-bool validateInputLine(const std::string& line, std::string& date, float& value) {
-    std::stringstream ss(line);
-    std::string separator;
+void    initialParse(int argc, char** argv)
+{
+	if (argc != 2) {
+		std::cerr << "Usage: btc <input_file>\n";
+		exit (1);
+	}
+	
+	std::ifstream inputFile(argv[1]);
+	if (!inputFile.is_open()) {
+		std::cerr << "Error opening file: " << argv[1] << '\n';
+		exit (1);
+	}
+	inputFile.close();
+	return ;
+}
 
-    std::getline(ss, date, '|');
-    if (ss.fail()) {
-        return false;
-    }
-    ss >> std::ws; 
-    ss >> value;
-    if (ss.fail() || value < 0 || value > 1000) {
-        return false;
-    }
+std::map<std::string, double> makeBitcoinMap(const std::string& filename) {
+	std::ifstream dataFile(filename.c_str());
+	std::map<std::string, double> bitcoinMap;
 
-    std::tm tm = {};
-    memset(&tm, 0, sizeof(tm));
-    if (strptime(date.c_str(), "%Y-%m-%d", &tm) == NULL) {
-        return false;
-    }
+	if (!dataFile.is_open()) {
+		std::cerr << "Error reading data.csv\n";
+		exit (1);
+	}
 
-    std::time_t input_date = std::mktime(&tm);
-    std::tm min_date_tm = {};
-    memset(&min_date_tm, 0, sizeof(min_date_tm));
-    min_date_tm.tm_year = 2009 - 1900;
-    min_date_tm.tm_mon = 1 - 1;
-    min_date_tm.tm_mday = 2;
-    std::time_t min_date = std::mktime(&min_date_tm);
+	std::string line;
+	while (std::getline(dataFile, line)) {
+		std::stringstream ss(line);
+		std::string date;
+		double value;
 
-    if (input_date < min_date) {
-        return false;
-    }
+		std::getline(ss, date, ',');
+		ss >> value;
 
-    return true;
+		bitcoinMap[date] = value;
+	}
+	dataFile.close();
+
+	return bitcoinMap;
+}
+
+std::string findClosestDate(const std::map<std::string, double>& data, const std::string& targetDate) 
+{
+
+	if (data.find(targetDate) != data.end()) {
+		return targetDate;
+	}
+
+	std::map<std::string, double>::const_iterator upperBound = data.upper_bound(targetDate);
+
+	if (upperBound == data.begin()) {
+		return upperBound->first;
+	}
+	--upperBound;
+	return upperBound->first;
+}
+
+bool	checkValue(const float& value)
+{
+	if (value < 0){
+		std::cerr << "Error: not a positive number" << '\n';
+		return (false);
+	}
+	if (value > 1000){
+		std::cerr << "Error: too large number" << '\n';
+		return (false);
+	}
+	return (true);
+}
+
+bool validateInputLine(const std::string& line, std::string& date, float& value) 
+{
+	std::stringstream ss(line);
+	std::string separator;
+
+	std::getline(ss, date, '|');
+	if (ss.fail()) {
+		std::cerr << "Error: bad input => " << line << '\n';
+		return false;
+	}
+
+	ss >> std::ws; 
+	ss >> value;
+
+	if (ss.fail()) {
+		std::cerr << "Error: bad input => " << line << '\n';
+		return false;
+	}
+
+	if (checkValue (value) == false)
+		return false;
+
+	std::tm tm = {};
+	memset(&tm, 0, sizeof(tm));
+	if (strptime(date.c_str(), "%Y-%m-%d", &tm) == NULL) {
+		std::cerr << "Error: invalid date " << date << '\n';
+		return false;
+	}
+
+	std::time_t input_date = std::mktime(&tm);
+	std::tm min_date_tm = {};
+	const char* min_date_str = "2009-01-02";
+	if (strptime(min_date_str, "%Y-%m-%d", &min_date_tm) == NULL) {
+		std::cerr << "Error: A runtime problem occurred" << date << '\n';
+		return false;
+	}
+
+	std::time_t min_date = std::mktime(&min_date_tm);
+	if (input_date < min_date) {
+		std::cerr << "Error: invalid date. Must be after \"2009-01-02\" your date: " << date << '\n';
+		return false;
+	}
+	return true;
 }
--- module09/ex00/main.cpp
+++ module09/ex00/main.cpp
@@ -1,43 +1,12 @@
 #include "BitcoinExchange.hpp"
 
 
-int main(int argc, char* argv[]) {
+int main(int argc, char** argv)
+{
 
- if (argc != 2) {
-        std::cerr << "Usage: btc <input_file>\n";
-        return 1;
-    }
-    
-    std::ifstream inputFile(argv[1]);
-    if (!inputFile.is_open()) {
-        std::cerr << "Error opening file: " << argv[1] << '\n';
-        return 1;
-    }
-
-    std::string line;
-
-    std::map<std::string, double> bitcoinPriceData = readBitcoinPriceData("data.csv");
-    if (bitcoinPriceData.empty()) {
-        std::cerr << "Error reading data.csv\n";
-        return 1;
-    }
-
-    while (std::getline(inputFile, line)) {
-        std::string date;
-        float value;
-
-        if (validateInputLine(line, date, value)) {
-            std::string closestDate = findClosestDate(bitcoinPriceData, date);
-            double exchangeRate = bitcoinPriceData[closestDate];
-            double result = value * exchangeRate;
-            std::cout << date << " => " << value << " = " << result << '\n';
-        } else {
-            std::cerr << "Error: bad input => " << line << '\n';
-        }
-    }
-    inputFile.close();
-
-    return 0;
+    initialParse (argc, argv);
+    bitcoinExchange (argv[1]);
+    return (0);
 }
 
 
